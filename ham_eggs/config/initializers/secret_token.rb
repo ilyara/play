a1fc72edaebb73bbe2fa0f820c98da217fe573ad
ilyara <ilya@trinix.com>
2011-03-08 15:45:34 -0500

    haml & eggs

---
+++ ham_eggs/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HamEggs::Application.config.secret_token = 'd28247fa47a2011b83a10b261ee1e2548b41a997e8db29db83d3e5e59958c641898e70d6bb18e08b2a12b870ad8c4310690bf4cbe95331252414a6398af3c365'
